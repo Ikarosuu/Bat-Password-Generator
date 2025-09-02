7fb09c00ef4acd8c060912978d0a4f322cc54d93
ikarosuu <icaro.code@proton.me>
2025-09-02 14:53:16 -0400

    separando as responsabilidades

---
+++ src/screens/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar';
+import styles from '../screens/Style'
+import { BatLogo } from '../components/BatLogo/BatLogo';
+
+export default function Home(){
+    return(
+        <View style={styles.container}>
+            <View>
+                <BatLogo/>
+            </View>
+            
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    )
+}
