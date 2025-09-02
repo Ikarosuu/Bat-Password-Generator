0bd740bb0611e7a6cf71b0535998268c42500971
ikarosuu <icaro.code@proton.me>
2025-09-02 16:36:40 -0400

    finalizando a parte logica

--- src/components/BatButton/BatButton.tsx
+++ src/components/BatButton/BatButton.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import { Button, View, Text, Pressable } from 'react-native';
+import { Text, Pressable } from 'react-native';
 
 import { styles } from './BatButtonStyles';
 import { BatTextInput } from '../BatTextInput/BatTextInput';
 import generatePass from '../../services/passwordService';
+
+import * as Clipboard from 'expo-clipboard'
 
 export function BatButton() {
   const [pass, setPass] = useState('')
@@ -12,13 +14,17 @@ export function BatButton() {
     let generateToken = generatePass()
     setPass(generateToken)
   }
+
+  function handleCopyButton(){
+    Clipboard.setStringAsync(pass)
+  }
   return (
     <>
       <BatTextInput pass={pass}/>
       <Pressable onPress={handleGenerateButton} style={styles.button}>
           <Text style={styles.text}>GENERATE</Text>
       </Pressable>
-      <Pressable style={styles.button}>
+      <Pressable onPress={handleCopyButton} style={styles.button}>
           <Text style={styles.text}>⚡COPY</Text>
       </Pressable>
     </>
